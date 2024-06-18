e1e16f532bf6782b017d450d53fb9ae47f781bd0
Eric Nguyen <eric.nguyen103@gmail.com>
2024-06-17 21:02:12 -0400

    updated paths to test for deployment

--- src/app/OnBoarding/page.jsx
+++ src/app/OnBoarding/page.jsx
@@ -1,44 +1,46 @@
 "use client";
 
-import React, { userState} from 'react';
+import React, { useState } from 'react';
 import { auth, db } from "../../firebase";
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore"; 
+import { doc, setDoc } from "firebase/firestore";
 
 const Page = () => {
-    // Variables needed for the sign up form 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-    // Function to handle the sign up form
+
     const handleSignUp = async () => {
         try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password); // Create the user
-        const user = userCredential.user; // Get the user 
-        await setDoc(doc(db, "users", user.uid), { // Add the user to the database
-            email: user.email,
-        });
-        } catch (error) { 
-        setError(error.message);
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            await setDoc(doc(db, "users", user.uid), {
+                email: user.email,
+            });
+        } catch (error) {
+            setError(error.message);
         }
     };
-    
+
     return (
         <div>
-        <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleSignUp}>Sign Up</button>
-        {error && <p>{error}</p>}
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleSignUp}>Sign Up</button>
+            {error && <p>{error}</p>}
         </div>
     );
-    }
+};
+
+export default Page;
